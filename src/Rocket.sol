0c40c71e3bd6386068e2bd7b6c653325898732a5
Mubaris NK <hello@mubaris.com>
2022-08-31 18:39:28 +0400

    🏹 init

---
+++ src/Rocket.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import { IERC20 } from "./interfaces/IERC20.sol";
+import { IPolynomialVault } from "./interfaces/IPolynomialVault.sol";
+
+contract Rocket {
+    bytes32 public immutable salt;
+
+    constructor(bytes32 _salt) {
+        salt = _salt;
+    }
+
+    function launch(
+        address incomingToken,
+        address depositToken,
+        address vault,
+        address user,
+        uint256 amount,
+        address swapTarget,
+        bytes memory swapData
+    ) external {
+        require(
+            salt == keccak256(abi.encode(incomingToken, depositToken, vault, user, amount, swapTarget, swapData)),
+            "SALT_MISMATCH"
+        );
+
+        if (incomingToken != depositToken) {
+            require(swapTarget != address(0x0) && swapData.length > 0, "INVALID_REQUEST");
+
+            IERC20(incomingToken).approve(swapTarget, amount);
+            (bool success, ) = swapTarget.call(swapData);
+            require(success);
+        }
+
+        uint256 depositAmount = IERC20(depositToken).balanceOf(address(this));
+        IERC20(depositToken).approve(vault, depositAmount);
+        IPolynomialVault(vault).initiateDeposit(user, depositAmount);
+    }
+}
---
+++ src/RocketFactory.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import { Rocket } from "./Rocket.sol";
+
+contract RocketFactory {
+    function deploy(
+        address incomingToken,
+        address depositToken,
+        address vault,
+        address user,
+        uint256 amount,
+        address swapTarget,
+        bytes memory swapData
+    ) public payable returns (address) {
+        bytes32 uniqueSalt =
+            keccak256(abi.encode(incomingToken, depositToken, vault, user, amount, swapTarget, swapData));
+        return address(new Rocket{salt:  uniqueSalt}(uniqueSalt));
+    }
+
+    function getAddressFor(
+        address incomingToken,
+        address depositToken,
+        address vault,
+        address user,
+        uint256 amount,
+        address swapTarget,
+        bytes memory swapData
+    )
+        external
+        view
+        returns (address)
+    {
+        bytes32 salt =
+            keccak256(abi.encode(incomingToken, depositToken, vault, user, amount, swapTarget, swapData));
+        address predictedAddress = address(
+            uint160(
+                uint256(
+                    keccak256(
+                        abi.encodePacked(
+                            bytes1(0xff),
+                            address(this),
+                            salt,
+                            keccak256(abi.encodePacked(type(Rocket).creationCode, abi.encode(salt)))
+                        )
+                    )
+                )
+            )
+        );
+
+        return predictedAddress;
+    }
+}
